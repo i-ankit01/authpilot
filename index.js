fdec6303862bf56fa44b489d9501fe5032fd6bc2
i-ankit01 <ankit.common.acc@gmail.com>
2025-10-26 23:52:39 +0530

    update:configurations

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
-import { intro, confirm, spinner, outro, select } from "@clack/prompts";
+import {
+  intro,
+  confirm,
+  spinner,
+  outro,
+  select,
+  multiselect,
+} from "@clack/prompts";
 import { exec } from "child_process";
 import { promises as fs } from "fs";
 import { promisify } from "util";
@@ -21,8 +28,8 @@ function cancelFunction(value) {
 
 async function hasSrcDirectory() {
   try {
-    const stats = await fs.stat("src"); 
-    return stats.isDirectory(); 
+    const stats = await fs.stat("src");
+    return stats.isDirectory();
   } catch (err) {
     return false;
   }
@@ -40,6 +47,44 @@ async function overrideSchema(srcExists, dbType) {
   const templateContent = await fs.readFile(templatePath, "utf-8");
   await fs.writeFile(schemaPath, templateContent);
 }
+
+
+
+function configureImportName(str) {
+  return str+"Provider";
+}
+function buildProvidersCode(selected) {
+  const importLines = selected.map(
+    (p) => `import ${configureImportName(p)} from "next-auth/providers/${p.toLowerCase()}"`
+  );
+
+  const providerInstances = selected.map(
+    (p) => `${configureImportName(p)}({
+      clientId: process.env.${p.toUpperCase()}_CLIENT_ID!,
+      clientSecret: process.env.${p.toUpperCase()}_CLIENT_SECRET!,
+    })`
+  );
+
+  return `
+${importLines.join("\n")}
+import type { NextAuthConfig } from "next-auth";
+
+export default {
+  providers: [
+    ${providerInstances.join(",\n    ")}
+  ],
+} satisfies NextAuthConfig;
+`;
+}
+
+
+async function generateAuthConfig(selectedProviders) {
+  const code = buildProvidersCode(selectedProviders);
+
+  const targetPath = path.join(process.cwd(), "auth.config.ts");
+  await fs.writeFile(targetPath, code);
+}
+
 
 async function main() {
   intro(chalk.cyan(`${figures.play} AuthPilot CLI`));
@@ -80,6 +125,32 @@ async function main() {
     s.stop(chalk.green(`${figures.tick} Updated schema.prisma!`));
   } catch (error) {
     s.stop(chalk.red(`${figures.cross} Update failed.`));
+    outro(chalk.red(err.message));
+    process.exit(1);
+  }
+
+  const providers = await multiselect({
+    message: "Select the authentication providers you want to use:",
+    options: [
+      { value: "Credentials", label: "Credentials" },
+      { value: "Google", label: "Google", hint: "recommended" },
+      { value: "GitHub", label: "GitHub" },
+      { value: "Facebook", label: "FaceBook" },
+      { value: "Discord", label: "Discord" },
+      { value: "LinkedIn", label: "LinkedIn" },
+      { value: "Reddit", label: "Reddit" },
+      { value: "Twitter", label: "Twitter" },
+      { value: "Twitch", label: "Twitch" },
+    ],
+    required: false,
+  });
+
+  try {
+    s.start("Creating auth.config.ts");
+    await generateAuthConfig(providers)
+    s.stop(chalk.green(`${figures.tick} Created auth.config.ts file!`));
+  } catch (error) {
+    s.stop(chalk.red(`${figures.cross} Creation failed.`));
     outro(chalk.red(err.message));
     process.exit(1);
   }
